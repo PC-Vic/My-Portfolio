4c419ad7c006b0852ff2778f98aae46da94ed129
Victor Nieves <c_cc_vic@yahoo.com>
2024-03-22 11:59:24 -0400

    tested code for animation on main card

--- assets/script.js
+++ assets/script.js
@@ -19,3 +19,20 @@ document.querySelector('a[href="#contact"]').addEventListener('click', function 
     e.preventDefault();
     scrollToSection('#contact');
 });
+
+
+
+// fade in main card animation
+window.addEventListener('scroll', () => {
+    const card = document.getElementById('main-project-card');
+    
+    // Calculate the position of the card relative to the viewport
+    const rect = card.getBoundingClientRect();
+    const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+  
+    // If the top of the card is within the viewport, fade it in
+    if (rect.top <= windowHeight) {
+      card.style.opacity = 1;
+    }
+  });
+  
